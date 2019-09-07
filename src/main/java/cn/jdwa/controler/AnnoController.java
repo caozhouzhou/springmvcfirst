8ca4ac8b7659bc05dab3a2343dd5ea7ef637d78f
caozhou_zch <caozhou_zch@163.com>
2019-09-07 16:15:21 +0800

    testPathVariable

--- src/main/java/cn/jdwa/controler/AnnoController.java
+++ src/main/java/cn/jdwa/controler/AnnoController.java
@@ -1,6 +1,7 @@
 package cn.jdwa.controler;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +24,14 @@ public class AnnoController {
     public String testRequestBody(@RequestBody String body){
         System.out.println("执行了。。。");
         System.out.println(body);
+        return "success";
+    }
+
+    @RequestMapping("/{user}/testPathVariable/{id}")
+    public String testPathVariable(@PathVariable("user") String user,@PathVariable("id")String id){
+        System.out.println("执行了。。。");
+        System.out.println(user);
+        System.out.println(id);
         return "success";
     }
 }
